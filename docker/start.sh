23bf52ce9ac46355dcdf14ee5f48c3b6de9e9a3c
Rodrigo <71358562+rod9000@users.noreply.github.com>
2026-09-17 02:00:21 -0300

    att
    att

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+rm -f /etc/apache2/mods-enabled/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.conf
+rm -f /etc/apache2/mods-enabled/mpm_worker.load /etc/apache2/mods-enabled/mpm_worker.conf
 sed -i "s/Listen .*/Listen ${PORT:-80}/" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \*:[0-9]*>/<VirtualHost *:${PORT:-80}>/" /etc/apache2/sites-available/000-default.conf
 rm -f /var/www/html/bootstrap/cache/config.php
